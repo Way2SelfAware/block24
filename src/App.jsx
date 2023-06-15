8f941315fb03689c7ab1d9d4f0dd3a2fc5cb5dc6
Way2SelfAware <neo@DESKTOP-DDPIL1P>
2023-06-15 11:48:27 -0700

    added puppy array to console and mapped array to display puppy.name

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from "react";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   return (
     <>
-      <div></div>
+      <div>
+        {puppies.map((puppy) => {
+          return <p>{puppy.name}</p>;
+        })}
+      </div>
     </>
   );
 }
